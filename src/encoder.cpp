427827cd8cffb19183255b966fa0cf6a493760aa
AndBondStyle <andbondstyle@gmail.com>
2021-09-11 22:59:13 +0300

    Fix pole angle and pole velocity

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -10,11 +10,11 @@
 #include "protocol_processor.h"
 
 namespace {
-    const int ENCODER_MAX_VALUE = 4096;  // 12 bit
-    const unsigned long REFRESH_INTERVAL_MILLIS = 100;
-    const float ZERO_ANGLE = 1090;
-    const bool REVERSE = false;
-}
+const int ENCODER_MAX_VALUE = 4096;  // 12 bit
+const unsigned long REFRESH_INTERVAL_MILLIS = 100;
+const int ZERO_ANGLE = 1917;
+const bool REVERSE = true;
+}  // namespace
 
 Encoder::Encoder() : as5600(), prevAngle(0), prevTime(0) {
     ProtocolProcessor &P = GetProtocolProcessor();
@@ -27,13 +27,16 @@ void Encoder::Poll() {
     if (currTime - prevTime < REFRESH_INTERVAL_MILLIS) return;
     float rawAngle = as5600.getRawAngle();
     float currAngle = (rawAngle - ZERO_ANGLE) / ENCODER_MAX_VALUE * 2 * PI;
+    if (currAngle < 0) currAngle += 2 * PI;
     if (REVERSE) currAngle = 2 * PI - currAngle;
-    
+
     float deltaAngle = currAngle - prevAngle;
-    unsigned long deltaTime = currTime - prevTime;    
+    unsigned long deltaTime = currTime - prevTime;
     Globals &G = GetGlobals();
     G.pole_v = deltaAngle / deltaTime * 1000;
-    G.pole_x = prevAngle = currAngle;
+    G.pole_x = currAngle;
+    prevAngle = currAngle;
+    prevTime = currTime;
 }
 
 Encoder &GetEncoder() {
