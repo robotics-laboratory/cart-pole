285cd7986f22d5efa89a2ddcf5c73d5859c793e0
AndBondStyle <andbondstyle@gmail.com>
2021-09-19 22:02:31 +0300

    Reset firmware files to master version

--- src/protocol_processor.cpp
+++ src/protocol_processor.cpp
@@ -1,38 +1,40 @@
 #include "protocol_processor.h"
 
+#include <vector>
+
 #include "globals.h"
 #include "stepper.h"
 
-#include <vector>
-
 namespace {
-    const int SERIAL_SPEED = 115200;
+const HardwareSerial PROTOCOL_SERIAL_PORT = Serial;
+const int SERIAL_SPEED = 115200;
 
-    Globals &G = GetGlobals();
-    Stepper &S = GetStepper();
-}
+using ErrorEnum = Error;
+}  // namespace
 
-ProtocolProcessor::ProtocolProcessor() : serial_port(Serial) {
+ProtocolProcessor::ProtocolProcessor() : serial_port(PROTOCOL_SERIAL_PORT) {
     serial_port.begin(SERIAL_SPEED);
-    while (!serial_port) {}  // Wait for init
+    while (!serial_port) {
+    }  // Wait for init
     Log("CARTPOLE CONTROLLER STARTED");
 }
 
 void ProtocolProcessor::Poll() {
-    std::string buffer;
+    static std::string buffer;
     while (serial_port.available()) {
         char c = serial_port.read();
         if (c == '\n') {
+            handleCommand(buffer);
+            buffer.clear();
             break;
         }
         buffer += std::tolower(c);
     }
-
-    std::stringstream stream(buffer);
-    handleCommand(stream);
 }
 
-void ProtocolProcessor::handleCommand(std::stringstream &stream) {
+void ProtocolProcessor::handleCommand(const std::string &line) {
+    std::stringstream stream{line};
+
     std::string command;
     stream >> command;
 
@@ -45,14 +47,14 @@ void ProtocolProcessor::handleCommand(std::stringstream &stream) {
             result = get(group, stream);
         } else if (command == "set") {
             std::string group;
-            stream >> group;  
+            stream >> group;
             result = set(group, stream);
         } else if (command == "reset") {
             result = reset();
         } else {
             throw std::runtime_error{"Unknown command: " + command};
         }
-    } catch(std::exception &e) {
+    } catch (std::exception &e) {
         Error(e.what());
         return;
     }
@@ -64,40 +66,37 @@ void ProtocolProcessor::Success(const std::string &text) {
     serial_port.printf("+ %s\n", text.c_str());
 }
 
-void ProtocolProcessor::Log(const std::string &text) {
-    serial_port.printf("# %s\n", text.c_str());
-}
+void ProtocolProcessor::Log(const std::string &text) { serial_port.printf("# %s\n", text.c_str()); }
 
 void ProtocolProcessor::Error(const std::string &text) {
     serial_port.printf("! %s\n", text.c_str());
 }
 
-void ProtocolProcessor::KeepAlive() {
-    serial_port.printf("~\n");
-}
+void ProtocolProcessor::KeepAlive() { serial_port.printf("~\n"); }
 
 std::string ProtocolProcessor::get(const std::string &group, std::stringstream &stream) {
+    Globals &G = GetGlobals();
     std::stringstream result;
 
     std::string key;
     bool first = true;
     while (stream >> key) {
-        result << key << "=" << G.Get(group, key);
         if (first) {
             first = false;
         } else {
             result << ' ';
         }
+        result << key << "=" << G.Get(group, key);
     }
     if (first) {
         auto res = G.Get(group);
         for (auto &&kv : res) {
-            result << kv.first << ' ' << kv.second;
             if (first) {
                 first = false;
             } else {
                 result << ' ';
             }
+            result << kv.first << '=' << kv.second;
         }
     }
 
@@ -105,6 +104,11 @@ std::string ProtocolProcessor::get(const std::string &group, std::stringstream &
 }
 
 std::string ProtocolProcessor::set(const std::string &group, std::stringstream &stream) {
+    Globals &G = GetGlobals();
+    if (G.errcode != ErrorEnum::NO_ERROR) {
+        throw std::runtime_error{"Global error code is set"};
+    }
+
     std::stringstream result;
 
     std::vector<std::string> request_keys;
@@ -124,25 +128,34 @@ std::string ProtocolProcessor::set(const std::string &group, std::stringstream &
     bool first = true;
     for (const auto &key : request_keys) {
         G.Commit(group, key);
-        result << key << "=" << G.Get(group, key);
         if (first) {
             first = false;
         } else {
             result << ' ';
         }
+        result << key << '=' << G.Get(group, key);
     }
 
     return result.str();
 }
 
 std::string ProtocolProcessor::reset() {
+    Stepper &S = GetStepper();
+    Globals &G = GetGlobals();
+
     S.AsyncHoming();
     while (!S.IsDoneHoming()) {
         KeepAlive();
-        delay(500);
+        delay(100);
     }
+    KeepAlive();
+    delay(100);
 
+    float hw_max_x = G.hw_max_x;
     G.Reset();
+    G.errcode = ErrorEnum::NO_ERROR;
+    G.hw_max_x = hw_max_x;
+    G.max_x = G.hw_max_x;
 
     return "";
 }
