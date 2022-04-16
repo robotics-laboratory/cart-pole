1c56be692f58e0e96f3274809aa928675b3e3897
Andrey.Bondarev <Andrey.Bondarev@mvideo.ru>
2022-04-16 20:20:11 +0300

    Project structure fixes

--- scripts/build_proto.py
+++ scripts/build_proto.py
@@ -2,21 +2,18 @@ from pathlib import Path
 import subprocess
 import sys
 
-Import("env")
-
 NANOPB_ROOT = Path(".pio/libdeps/esp32dev/Nanopb")
-PROTO_FILE = Path("brain_controller.proto")
-C_OUTPUT_DIR = "src"
-PY_OUTPUT_DIR = "cartpole/device"
+PROTO_FILE = Path("protocol.proto")
+C_OUTPUT_DIR = Path("firmware/src")
+PY_OUTPUT_DIR = Path("cartpole/device")
 
-print("KEK" * 20)
 subprocess.check_call([
     sys.executable, 
     NANOPB_ROOT / "generator" / "protoc",  
     "-I", NANOPB_ROOT / "generator" / "proto",
     "-I", PROTO_FILE.parent,
-    # "-I.", 
     "--nanopb_out", C_OUTPUT_DIR, 
     "--python_out", PY_OUTPUT_DIR, 
     PROTO_FILE
 ])
+print("[build_proto.py] Protobuf bindings generated")
