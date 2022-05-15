942af79864aa55818e1267bfc625f183787b0856
Andrew Bond <andbondstyle@gmail.com>
2022-05-15 17:04:47 +0300

    Uncommited changes

--- control/calibration.py
+++ control/calibration.py
@@ -8,37 +8,44 @@ from math import sin, cos
 def acc_change_duration(a0, l0, moves_num):
     t0 = (l0 / a0) ** 0.5
     
-    change_timestamps = [t0]
+    sign_change_timestamps = [t0]
+    abs_change_timestamps = [2*t0]
     
     for i in range(moves_num):
         timestamp = t0 * ((2 / (i + 2)) ** 0.5)
-        change_timestamps.append(timestamp) 
+        sign_change_timestamp = abs_change_timestamps[-1] + timestamp
+        abs_change_timestamp = sign_change_timestamps[-1] + timestamp
+        sign_change_timestamps.append(sign_change_timestamp)
+        abs_change_timestamps.append(abs_change_timestamp)
     
-    return change_timestamps
+    return sign_change_timestamps, abs_change_timestamps
 
 
 def collect_data(config, cart_pole, moves_num):
     a0 = a = config.max_acceleration / ((moves_num + 1) * 1.1)
-    l0 = config.max_position * 0.9
-    change_timestamps = acc_change_duration(a0, l0, moves_num)
+    l0 = config.max_position * 0.7
+    sign_change_timestamps, abs_change_timestamps = acc_change_duration(a0, l0, moves_num)
     
     data = []
-    cart_pole.reset(config)
+    k = 1
+    # cart_pole.reset(config)
     start_time = cart_pole.timestamp()
     
-    while change_timestamps:
+    while abs_change_timestamps:
         time = cart_pole.timestamp() - start_time
         state = cart_pole.get_state()
         data.append({"state": state, "time": time, "target": a})
-        
-        if time > change_timestamps[0]:
+
+        if time > abs_change_timestamps[0]:
+            a *= (k + 1) / k
+            k += 1
+            abs_change_timestamps.pop(0)
+
+        elif time > sign_change_timestamps[0]:
             a = -a
-            change_timestamps.pop(0)
+            sign_change_timestamps.pop(0)
             
         cart_pole.set_target(a)
-        timestepp = round(random.uniform(0.008, 0.012), 10)
-        cart_pole.advance(timestepp)
-        print(time, timestepp, time + timestepp)
         
     return data
      
@@ -68,6 +75,57 @@ def execute_parameters(data, config):
 def calibrate(config, cart_pole, moves_num = 18):
     data = collect_data(config, cart_pole, moves_num)
     config.pole_length = execute_parameters(data, config)
-    cart_pole.reset(config)
+    # cart_pole.reset(config)
     
-    return config, cart_pole
+    return config.pole_length
+
+
+if __name__ == '__main__':
+    from common.util import init_logging
+    from device import CartPoleDevice
+    from sessions.collector import CollectorProxy
+    from pathlib import Path
+    import time
+    import logging
+    init_logging()
+    LOGGER = logging.getLogger('debug-session-runner')
+
+    SESSION_ID = 'calibration_test_1'
+    DEVICE_CONFIG = Config(
+        max_position=0.25,
+        max_velocity=5,
+        max_acceleration=10,
+        clamp_velocity=True,
+        clamp_acceleration=True,
+    )
+
+    SESSION_MAX_DURATION = 150.0
+    OUTPUT_PATH = Path(f'data/sessions/{SESSION_ID}')
+    init_logging()
+    device = CartPoleDevice()
+    # analyzer = SaleaeAnalyzer()
+    proxy = CollectorProxy(
+        cart_pole=device,
+        actor_class=object,
+        actor_config={},
+        # reset_callbacks=[analyzer.start],
+        # close_callbacks=[analyzer.stop],
+    )
+
+    try:
+        proxy.reset(DEVICE_CONFIG)
+        exit(0)
+        time.sleep(5.0)
+        # LOGGER.info(">>>")
+        # device.interface.set(DeviceTarget(position=0.05))
+        # LOGGER.info("<<<")
+        ret = calibrate(DEVICE_CONFIG, proxy)
+        print(ret)
+    except Exception:
+        LOGGER.exception('Aborting run due to error')
+    finally:
+        proxy.close()
+        LOGGER.info('Run finished')
+
+    proxy.save(OUTPUT_PATH / 'session.json')
+    # analyzer.save(OUTPUT_PATH)
