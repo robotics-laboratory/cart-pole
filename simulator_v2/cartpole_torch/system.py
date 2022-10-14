1e9792eff5a60456fde31c7cd586ba778a52e2f1
Nikolay Chechulin <nchechulin@protonmail.com>
2022-10-14 14:31:31 +0300

    remove cartpole_system and history

--- simulator_v2/cartpole_torch/system.py
+++ simulator_v2/cartpole_torch/system.py
@@ -1,143 +1,19 @@
 """
-This module contains 2 classes which both represent CartPole systems.
-They are responsible for simulating the physics, although they have some
-differences:
-- `CartPoleSystem` stores all the information about one system and
-    evaluates the derivatives to get new state. It cannot calculate best
-    inputs (for now).
-- `CartPoleMultiSystem` has a different approach. The systems data is stored
-    in a `MultiSystemLearningContext` and the system itself only performs
-    neccessary calculations for multiple CartPole systems at a time.
-    It provides a "functional" interface which allows us to change contexts
-    on fly if needed.
+This module contains CartPoleMultiSystem class. The systems data is stored
+in a `MultiSystemLearningContext` and the system itself only performs
+neccessary calculations for multiple CartPole systems at a time.
+It provides a "functional" interface which allows us to change contexts
+on fly if needed.
 
-    It supports evaluating best inputs for a given set of states and also
-    evaluating new states (after inputs are applied).
+It supports evaluating best inputs for a given set of states and also
+evaluating new states (after inputs are applied).
 """
 
-
-from dataclasses import dataclass
 
 import torch
 from torch import DoubleTensor, cos, sin
 
-from .config import SystemConfiguration
-from .history import SystemHistory
 from .learning_context import MultiSystemLearningContext
-from .state import State
-
-# FIXME: CartPoleSystem and CartPoleMultiSystem are not interchangable.
-# Think of a way to create some kind of a base class
-# Or simply say that CartPoleSystem is a MultiSystem with 1 state.
-
-
-@dataclass
-class CartPoleSystem:
-    """
-    Defines the system as a whole
-
-    Fields
-    ------
-    `config` : SystemConfiguration
-        Physical configuration of the system.
-
-    `current_state` : State
-        Current state of the system.
-        Changes over time.
-
-    `target_state` : State
-        State we want the system to be in.
-        Should remain constant.
-
-    `simulation_time` : float
-        Time (in seconds) since the start of the simulation.
-    """
-
-    config: SystemConfiguration = SystemConfiguration()
-    current_state: State = State.home()
-    # TODO: remove field and use State.target() to ensure immutability
-    # TODO: think of cases when the field should be mutable
-    target_state: State = State.target()
-    simulation_time: float = 0.0
-    history: SystemHistory = SystemHistory()
-    # Pre-allocated tensors which are often used
-    # ds1 and ds2 are used for derivative computation
-    _ds1: DoubleTensor = None  # type: ignore # delta state 1
-    _ds2: DoubleTensor = None  # type: ignore # delta state 2
-
-    def __post_init__(self) -> None:
-        self._ds1 = DoubleTensor([0, 0, 0, 0])
-        self._ds2 = DoubleTensor([0, 0, 0, 0])
-
-    def advance_to(self, target_time: float, best_input: float) -> None:
-        """
-        Advances the system to a given moment in time.
-
-        Parameters
-        ----------
-        `target_time` : float
-            New time of the system.
-            Should be greater than `simulation_time`.
-        `best_input` : float
-            Input to the system (in m/s^2).
-            Will be removed.
-
-        Raises
-        ------
-        ValueError
-            If target time is smaller than `simulation_time`.
-        """
-        if target_time < self.simulation_time:
-            raise ValueError("Target time should be greater than current time")
-        # FIXME: Remove best input from args and calculate it each time
-        while self.simulation_time < target_time:
-            self.advance_one_step(best_input)
-
-    def advance_one_step(self, best_input: float) -> None:
-        """
-        Advances the system one step further.
-        One step equals `config.input_timestep` seconds.
-        """
-        # Current state
-        cur_st: DoubleTensor = self.current_state.as_tensor()
-        steps: int = self.config.discretization.dynamics_steps_per_input
-        # Delta time
-        d_time: float = 1 / (steps * self.config.discretization.input_frequency)
-
-        grav: float = self.config.parameters.gravity  # Gravitational constant
-        pole_len: float = self.config.parameters.pole_length
-
-        def __compute_derivative(
-            state: DoubleTensor,
-            delta_state: DoubleTensor,
-        ) -> None:
-            ang = state[1]  # 1x1 Tensor with angle
-            delta_state[0] = state[2]
-            delta_state[1] = state[3]
-            delta_state[2] = best_input
-            delta_state[3] = (
-                (-3 / 2) * (best_input * cos(ang) + grav * sin(ang)) / pole_len
-            )
-
-        for _ in range(steps):
-            # Evaluate derivatives
-            __compute_derivative(
-                state=cur_st,
-                delta_state=self._ds1,
-            )
-            __compute_derivative(
-                state=cur_st + self._ds1 * d_time,  # type: ignore
-                delta_state=self._ds2,
-            )
-            cur_st += (self._ds1 + self._ds2) / 2 * d_time  # type: ignore
-
-        self.history.add_entry(
-            timestamp=self.simulation_time,
-            current_input=best_input,
-            state=self.current_state,
-        )
-        self.current_state = State.from_collection(cur_st)  # type: ignore
-        self.simulation_time += 1 / self.config.discretization.input_frequency
 
 
 class CartPoleMultiSystem:
