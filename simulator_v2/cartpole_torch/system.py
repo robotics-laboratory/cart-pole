3a67e3a8b4ef29cff9c6d496074aa55b9c0b2407
Nikolay Chechulin <nchechulin@protonmail.com>
2022-11-15 13:42:57 +0300

    Sim v2 Stateful Simulator with Common Interface (#32)
    * create empty CartPoleSystem class
    
    * implement stateful simulator
    
    * add demo notebook for simulator v2
    
    * remove init since it is not needed
    
    * remove close method since gc will collect everything
    
    * make calculation of new states modify them in-place
    
    * remove unused import
    
    * implement errors

--- simulator_v2/cartpole_torch/system.py
+++ simulator_v2/cartpole_torch/system.py
@@ -10,10 +10,18 @@ evaluating new states (after inputs are applied).
 """
 
 
+from dataclasses import dataclass
+
 import torch
 from torch import DoubleTensor, cos, sin
 
+from common import CartPoleBase
+from common import Error
+
+from .config import SystemConfiguration
+from .discreditizer import Discreditizer
 from .learning_context import MultiSystemLearningContext
+from .state import MultiSystemState, State
 
 
 class CartPoleMultiSystem:
@@ -26,28 +34,20 @@ class CartPoleMultiSystem:
     """
 
     @staticmethod
-    def get_new_states(
+    def eval_transitions(
         context: MultiSystemLearningContext, inputs: DoubleTensor
-    ) -> DoubleTensor:
+    ) -> None:
         """
-        Reterns positions after applying `inputs`
+        Calculates new states and writes them in-place.
 
         Parameters
         ----------
         context : MultiSystemLearningContext
         inputs : DoubleTensor
             A 1xN tensor containing input cart accelerations.
-
-        Returns
-        -------
-        DoubleTensor
-            A 4xN Tensor containing N states after an input tick.
-            By default input tick is
-            `1 / context.config.discretization.input_time`
         """
         # Current state
         cur_st = context.batch_state.states
-        cur_st: DoubleTensor = torch.clone(cur_st)  # type: ignore
         steps: int = context.config.discretization.integration_step_n
         # dynamics delta time
         d_time: float = 1 / (steps * context.config.discretization.simulation_step_n)
@@ -70,16 +70,13 @@ class CartPoleMultiSystem:
 
         for _ in range(steps):
             # Evaluate derivatives
-            ds1 = compute_derivative(cur_st)
+            ds1 = compute_derivative(cur_st)  # type: ignore
             ds2 = compute_derivative(cur_st + ds1 * d_time)  # type: ignore
             cur_st += (ds1 + ds2) / 2 * d_time  # type: ignore
-
-        return cur_st
 
     @staticmethod
     def eval_transition_costs(
         context: MultiSystemLearningContext,
-        new_states: DoubleTensor,
         inputs: DoubleTensor,
     ) -> DoubleTensor:
         """
@@ -88,8 +85,6 @@ class CartPoleMultiSystem:
         Parameters
         ----------
         context : MultiSystemLearningContext
-        new_states : DoubleTensor
-            States after applying inputs (after transition).
         inputs : DoubleTensor
             The inputs applied
 
@@ -99,15 +94,13 @@ class CartPoleMultiSystem:
             1xK Tensor containing total cost of applying
             i-th action to i-th state.
         """
-        states_cost = context.states_cost_fn(new_states)
+        states_cost = context.states_cost_fn(context.batch_state.states)
         inputs_cost = context.inputs_cost_fn(inputs)
 
         return states_cost + inputs_cost  # type: ignore
 
     @staticmethod
-    def get_best_accelerations(
-        context: MultiSystemLearningContext,
-    ) -> DoubleTensor:
+    def get_best_accelerations(context: MultiSystemLearningContext) -> DoubleTensor:
         """
         Returns best input for each state.
 
@@ -136,10 +129,9 @@ class CartPoleMultiSystem:
                 dtype=torch.float64,
             )  # type: ignore
 
-            new_states = CartPoleMultiSystem.get_new_states(context, inputs)
+            CartPoleMultiSystem.eval_transitions(context, inputs)
             costs = CartPoleMultiSystem.eval_transition_costs(
                 context,
-                new_states,
                 inputs,
             )
 
@@ -154,3 +146,123 @@ class CartPoleMultiSystem:
             best_inputs[better_mask] = acc
 
         return best_inputs
+
+
+@dataclass(init=False)
+class CartPoleSystem(CartPoleBase):
+    """
+    Description:
+        Сlass implements a physical simulation of the cart-pole device.
+        A pole is attached by an joint to a cart, which moves along guide axis.
+        The pendulum is initially at rest state. The goal is to maintain it in
+        upright pose by increasing and reducing the cart's velocity.
+    Source:
+        This environment is some variation of the cart-pole problem
+        described by Barto, Sutton, and Anderson
+    Initial state:
+        A pole is at starting position 0 with no velocity and acceleration.
+    """
+
+    _context: MultiSystemLearningContext
+    _current_input: float = 0
+    _config: SystemConfiguration = SystemConfiguration()
+    _current_time: float = 0
+    _error: Error = Error.NO_ERROR
+
+    def _setup_context(
+        self,
+        state: State,
+    ) -> None:
+        batch_state = state.as_tensor().reshape(4, -1)
+        batch_ms_state = MultiSystemState(batch_state)  # type: ignore
+        self._context = MultiSystemLearningContext(
+            states_cost_fn=None,  # type: ignore
+            inputs_cost_fn=None,  # type: ignore
+            config=self._config,
+            discreditizer=Discreditizer(self._config),
+            batch_state=batch_ms_state,
+        )
+
+    def reset(self, config: SystemConfiguration) -> None:
+        """
+        Resets the device to the initial state.
+        The pole is at rest position and cart is centered.
+        It must be called at the beginning of any session.
+        """
+        self._setup_context(State.home())
+
+        self._config = config
+        self._current_time = 0
+
+    def reset_to_state(self, config: SystemConfiguration, state: State) -> None:
+        self._setup_context(state)
+
+        self._config = config
+        self._current_time = 0
+
+    def get_state(self) -> State:
+        """
+        Returns current device state.
+        """
+        state_tensor = self._context.batch_state.states.flatten()
+        return State.from_collection(state_tensor)  # type: ignore
+
+    def get_info(self) -> dict:
+        """
+        Returns usefull debug information.
+        """
+        raise NotImplementedError
+
+    def get_target(self) -> float:
+        """
+        Returns current target acceleration.
+        """
+        return self._current_input
+
+    def set_target(self, target: float) -> None:
+        """
+        Set desired target acceleration.
+        """
+        max_target = self._config.limits.max_abs_acceleration
+        if not -max_target <= target <= max_target:
+            self._error = Error.A_OVERFLOW
+            return
+
+        self._current_input = target
+
+    def advance(self, delta: float) -> None:
+        """
+        Advance the dynamic system by delta seconds.
+        """
+        if self._error != Error.NO_ERROR:
+            print(f"Error occurred: {self._error}. Simulation is stopped")
+            return
+
+        max_x = self._config.limits.max_abs_position
+        max_v = self._config.limits.max_abs_velocity
+
+        sim_steps = self._config.discretization.simulation_step_n
+        for _ in range(int(delta * sim_steps)):
+            CartPoleMultiSystem.eval_transitions(
+                context=self._context,
+                inputs=torch.full(
+                    size=(1,),
+                    fill_value=self._current_input,  # type: ignore
+                    dtype=torch.float64,
+                ),
+            )
+
+            # Check that all the limits are ok
+            state = self._context.batch_state.states[:, 0]
+            if not -max_x <= state[0] <= max_x:
+                self._error = Error.X_OVERFLOW
+            if not -max_v <= state[1] <= max_v:
+                self._error = Error.V_OVERFLOW
+
+        self._current_time += delta
+
+    def timestamp(self) -> float:
+        """
+        Current time.
+        """
+        return self._current_time
