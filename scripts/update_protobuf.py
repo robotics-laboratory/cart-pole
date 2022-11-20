8d6eb8fea2654c69c99ffe3bcc811c403a85e742
AndBondStyle <andbondstyle@gmail.com>
2022-11-20 21:34:03 +0300

    Move firmware to separate dir, add protobuf generation script, update platformio, gitignore

---
+++ scripts/update_protobuf.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import subprocess as sp
+import sys
+
+NANOPB_ROOT = Path(".pio/libdeps/esp32dev/Nanopb")
+PROTO_FILE = Path("protocol.proto")
+C_OUTPUT_DIR = Path("firmware/src")
+PY_OUTPUT_DIR = Path("cartpole/device")
+
+cmd = [
+    f"{sys.executable}",
+    f"{NANOPB_ROOT / 'generator' / 'protoc'}",
+    f"-I {NANOPB_ROOT / 'generator' / 'proto'}",
+    f"-I {PROTO_FILE.parent}",
+    f"--nanopb_out {C_OUTPUT_DIR}",
+    f"--python_out {PY_OUTPUT_DIR}",
+    f"{PROTO_FILE}",
+]
+
+proc = sp.Popen(" ".join(cmd), shell=True, text=True, stdout=sp.PIPE, stderr=sp.PIPE)
+assert proc.wait() == 0, f"Protobuf compilation failed:\n{proc.stderr.read()}"
+print("[update_protobuf.py] Protobuf bindings generated")
