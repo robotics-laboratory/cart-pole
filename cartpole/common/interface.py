35c3009b2f2099543b35c939c06af6a500c2783b
AndBondStyle <andbondstyle@gmail.com>
2022-11-23 01:15:37 +0300

    Revert config defaults to master version

--- cartpole/common/interface.py
+++ cartpole/common/interface.py
@@ -21,18 +21,18 @@ class Error(enum.IntEnum):
 class Config:
     # software cart limits
     max_position: float = 0.25  # m
-    max_velocity: float = 25.0  # m/s
-    max_acceleration: float = 7  # m/s^2
+    max_velocity: float = 2.0  # m/s
+    max_acceleration: float = 3.5  # m/s^2
     # hardware limits
-    hard_max_position: float = 4  # m
-    hard_max_velocity: float = 25  # m/s
-    hard_max_acceleration: float = 8  # m/s^2
+    hard_max_position: float = 0.27  # m
+    hard_max_velocity: float = 2.5  # m/s
+    hard_max_acceleration: float = 5.0  # m/s^2
     # hardware flags
     clamp_position: bool = False
     clamp_velocity: bool = False
     clamp_acceleration: bool = False
     # physical params
-    pole_length: float = 0.2  # m
+    pole_length: float = 0.3  # m
     pole_mass: float = 0.118  # kg
     gravity: float = 9.8  # m/s^2
 
