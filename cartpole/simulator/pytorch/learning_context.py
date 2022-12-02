fd2d38d4eab9ff96139f68dbeccaf8cf34aa4267
Nikolay Chechulin <nchechulin@protonmail.com>
2022-12-02 17:06:30 +0300

    add a field with indecies of items from current batch

--- cartpole/simulator/pytorch/learning_context.py
+++ cartpole/simulator/pytorch/learning_context.py
@@ -4,11 +4,11 @@ for all of the data which is used to train a model which
 works with batches (multiple systems at a time).
 """
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Callable
 
 import torch
-from torch import DoubleTensor, LongTensor
+from torch import DoubleTensor, IntTensor
 
 from .config import SystemConfiguration
 from .discreditizer import Discreditizer
@@ -29,6 +29,7 @@ class MultiSystemLearningContext:
     config: SystemConfiguration
     batch_state: MultiSystemState
     discreditizer: Discreditizer
+    batch_indecies: IntTensor = field(default=torch.empty(0, dtype=torch.int))  # type: ignore
 
     def update_batch(self, batch_size: int) -> None:
         """
@@ -50,7 +51,7 @@ class MultiSystemLearningContext:
         if not (0 < batch_size <= total_states):
             raise ValueError("Invalid batch size")
 
-        batch: LongTensor = torch.randint(
+        self.batch_indecies = torch.randint(
             low=0,
             high=total_states,
             size=(batch_size,),  # type: ignore
@@ -58,7 +59,7 @@ class MultiSystemLearningContext:
 
         self.batch_state = MultiSystemState.create_from_batch(
             self.discreditizer.all_states,
-            batch,
+            self.batch_indecies,  # type: ignore
         )
 
     @property
