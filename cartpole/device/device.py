e4dcbdeed0a99311a1c95bfd4037759178e303e4
AndBondStyle <andbondstyle@gmail.com>
2023-10-15 02:30:18 +0300

    Uncommited changes

--- cartpole/device/device.py
+++ cartpole/device/device.py
@@ -11,7 +11,7 @@ import logging
 import time
 from typing import NamedTuple, Optional, Union
 
-from betterproto import Message
+from betterproto import Casing, Message
 from pydantic import BaseModel, BaseSettings, FilePath
 from serial import Serial
 from serial.tools.list_ports import comports
@@ -22,8 +22,18 @@ from cartpole.device import framing
 
 
 class ConnectionSettings(BaseSettings):
+    """
+    Device connection settings. Can be set via environment variables, or overriden
+    directly by passing kwargs to CartPoleDevice constructor.
+
+    - serial_port: path to serial device (e.g. /dev/serial/by-id/xxx)
+    - serial_speed: serial baud rate (bits/s)
+    - serial_timeout: serial read timeout (s)
+    - hard_reset: whether to perform hard device reset upon connecting
+    """
+
     serial_port: FilePath = None
-    serial_speed: int = 3_000_000
+    serial_speed: int = 1000000
     serial_timeout: float = 0.1
     hard_reset: bool = True
 
@@ -34,10 +44,17 @@ class ConnectionSettings(BaseSettings):
 
 
 class TypeMapping(NamedTuple):
-    request_native_type: Optional[BaseModel]
-    request_proto_type: Optional[Message]
-    response_native_type: Optional[BaseModel]
-    response_proto_type: Optional[Message]
+    """
+    Helper structure to define request/response arguments and mapping between python
+    dataclasses (pydantic models) and protobuf (betterproto) messages. Setting some
+    argument to None means request/response does not accept (return) any data (e.g.
+    reset command).
+    """
+
+    req_model: Optional[BaseModel] = None
+    req_message: Optional[Message] = None
+    res_model: Optional[BaseModel] = None
+    res_message: Optional[Message] = None
 
 
 # TODO: Move to cartpole.common.interface?
@@ -53,33 +70,22 @@ class DeviceConfig(Config, proto.Config):
 
 class CartPoleDevice(CartPoleBase):
     REQUEST_MAPPING = {
-        RequestType.RESET: TypeMapping(
-            request_native_type=None,
-            request_proto_type=None,
-            response_native_type=None,
-            response_proto_type=None,
-        ),
+        # Reset command: accepts nothing, returns nothing
+        RequestType.RESET: TypeMapping(),
         # TODO: Custom DeviceState class?
-        RequestType.TARGET: TypeMapping(
-            request_native_type=Target,
-            request_proto_type=proto.Target,
-            response_native_type=State,
-            response_proto_type=proto.State,
-        ),
+        # Set target command: accepts target, returns state
+        RequestType.TARGET: TypeMapping(Target, proto.Target, State, proto.State),
+        # Set config command: accepts config, returns config
         RequestType.CONFIG: TypeMapping(
-            request_native_type=DeviceConfig,
-            request_proto_type=proto.Config,
-            response_native_type=DeviceConfig,
-            response_proto_type=proto.Config,
+            DeviceConfig, proto.Config, DeviceConfig, proto.Config
         ),
     }
-    RESET_DELAY = 0.1
+    RESET_DELAY = 0.1  # Delay fr
     HOMING_TIMEOUT = 10
 
     def __init__(self, **kwargs):
         cfg = ConnectionSettings.with_overrides(**kwargs)
-        if cfg.serial_port is None:
-            cfg.serial_port = self._detect_port()
+        cfg.serial_port = cfg.serial_port or self._detect_port()
         logging.info(f"Serial: {cfg.serial_port} @ {cfg.serial_speed} bits/s")
         self._port = Serial(
             port=cfg.serial_port,
@@ -110,15 +116,16 @@ class CartPoleDevice(CartPoleBase):
         time.sleep(self.RESET_DELAY)
         # TODO: Consistent restart marker?
         data = self._port.read_until(b"start")
-        logging.info(f"Purged data: {data.decode(errors='ignore')}")
+        self._port.read_all()
+        logging.debug(f"Purged data: {data.decode(errors='ignore')}")
 
     def _request(self, type: RequestType, payload: Union[BaseModel, Message] = None):
         logging.debug(f"SEND: {type!r} request with payload: {payload}")
         mapping = self.REQUEST_MAPPING[type]
         if payload is not None:
             if not isinstance(payload, Message):
-                payload = mapping.request_proto_type(**payload.dict())
-            assert isinstance(payload, mapping.request_proto_type)
+                payload = mapping.req_message(**payload.dict())
+            assert isinstance(payload, mapping.req_message)
         data = framing.encode(type)
         self._port.write(data)
         logging.debug(f"SEND (raw): {data}")
@@ -127,10 +134,10 @@ class CartPoleDevice(CartPoleBase):
         if not data:
             logging.error(f"{type!r} request timeout")
             raise TimeoutError
-        response_type, response = framing.decode(data, mapping.response_proto_type)
+        response_type, response = framing.decode(data, mapping.res_message)
         assert type == response_type, "Unexpected response type"
         if response is not None:
-            response = mapping.request_native_type(**response.as_dict())
+            response = mapping.res_model(**response.to_dict(Casing.SNAKE))
         logging.debug(f"RECV: {response_type} response with payload: {response}")
         return response
 
@@ -152,11 +159,13 @@ if __name__ == "__main__":
 
     init_logging()
 
-    iterations = 2000
+    iterations = 1000
     start = time.perf_counter()
     device = CartPoleDevice()
     for _ in range(iterations):
-        device._request(RequestType.RESET)
+        state = device._request(
+            RequestType.TARGET, Target(position=1, velocity=2, acceleration=3)
+        )
     duration = time.perf_counter() - start
     print(f"{duration / iterations:.3f} seconds per iteration")
     device._port.close()
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -7,7 +7,7 @@
 #include "cobs.h"
 #include "proto/protocol.pb.h"
 
-const uint32_t SERIAL_SPEED = 3000000;
+const uint32_t SERIAL_SPEED = 1000000;
 const uint8_t FRAME_DELIMITER = 0x00;
 
 uint8_t rx_buff[256];
@@ -22,19 +22,46 @@ void loop() {
     size_t length = Serial.readBytesUntil(FRAME_DELIMITER, rx_buff, sizeof(rx_buff));
     if (!length) return;
     cobs::decode(rx_buff, length);
-    RequestType type = static_cast<RequestType>(rx_buff[0]);
-    size_t payload_len = rx_buff[1];
-    if (type == RequestType_RESET) {
+    // uint8_t type = rx_buff[1];
+    uint8_t type = 2;
+    // size_t payload_len = rx_buff[1];
+    if (type == 0) {
         uint8_t tx_buff[256];
         tx_buff[0] = 0;                  // Reserved for COBS overhead byte
-        tx_buff[1] = RequestType_RESET;  // Request type
+        tx_buff[1] = type;               // Request type
         tx_buff[2] = 0;                  // Data length
         crc.restart();
-        crc.add(tx_buff + 1, 2);         // CRC8 for bytes 1-2
-        tx_buff[3] = crc.getCRC();
+        crc.add(tx_buff + 1, 2);         
+        tx_buff[3] = crc.getCRC();       // CRC8 for bytes 1-2
         tx_buff[4] = FRAME_DELIMITER;    // End of frame
         size_t tx_size = cobs::encode(tx_buff, 4);
         Serial.write(tx_buff, 5);
+    } else if (type == 2) {
+        uint8_t tx_buff[256];
+        tx_buff[0] = 0;
+        tx_buff[1] = type;
+        tx_buff[2] = 0;
+
+        // Data region starts at byte 3
+        // 5 extra bytes reserved for COBS, TYPE, LEN, CRC8, EOF
+        pb_ostream_t ostream = pb_ostream_from_buffer(tx_buff + 3, sizeof(tx_buff) - 5);
+        State state = State_init_zero;
+        state.cart_position = 123.0;
+        state.has_cart_position = true;
+        bool ok = pb_encode(&ostream, &State_msg, &state);
+
+        // Serial.printf("size: %d ", ostream.bytes_written);
+        // Serial.printf("ok: %d ", ok);
+        // Serial.write(FRAME_DELIMITER);
+        // return;
+
+        tx_buff[2] = ostream.bytes_written;
+        crc.restart();
+        crc.add(tx_buff + 1, 2 + ostream.bytes_written);
+        tx_buff[3 + ostream.bytes_written] = crc.getCRC();
+        tx_buff[4] = FRAME_DELIMITER;
+        size_t tx_size = cobs::encode(tx_buff, 4 + ostream.bytes_written);
+        Serial.write(tx_buff, 5 + ostream.bytes_written);
     } else {
         Serial.print("error");
         Serial.write(FRAME_DELIMITER);
